497247317ea5465b5c4db46865ee61697db0828e
ChristhianSM <christhian2524@gmail.com>
2026-02-06 20:46:29 -0500

    Filtros en queryStrings

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ app.get("/", (req, res) => {
 
 app.get("/category/:slug", async (req, res) => {
   const { slug: categorySlug } = req.params;
+  const { minPrice: minPriceQuery, maxPrice: maxPriceQuery } = req.query;
+
+  console.log({ minPriceQuery, maxPriceQuery });
+
+  // Validar los queries Strings
+  const minPrice = minPriceQuery ? Number(minPriceQuery) : -Infinity; // product.price > -Infinity
+  const maxPrice = maxPriceQuery ? Number(maxPriceQuery) : Infinity; // product.price < Infinity
 
   // Leer mi archivo data.json
   const dataJson = await fs.readFile(DATA_PATH, "utf-8");
@@ -49,18 +56,25 @@ app.get("/category/:slug", async (req, res) => {
   );
 
   if (!categoryFind) {
-    return res.status(404).render("404");
+    return res.status(404).render("404", {
+      namePage: "Error",
+    });
   }
 
   // Obtenemos todos los productos que tengan la categoria encontrada
   const productsFilter = products.filter(
-    (product) => product.categoryId === categoryFind.id,
+    (product) =>
+      product.categoryId === categoryFind.id &&
+      product.price / 100 >= minPrice &&
+      product.price / 100 <= maxPrice,
   );
 
   res.render("category", {
     namePage: categoryFind.name,
     category: categoryFind,
     products: productsFilter,
+    minPrice: minPriceQuery || "",
+    maxPrice: maxPriceQuery || "",
   });
 });
 
